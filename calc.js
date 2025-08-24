a3a6c7e1721e4ba49b796d648cf70d60282334fe
ojasvividvaan-eng <ojasvividvaan@gmail.com>
2025-08-24 16:20:55 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+   return(x + y);
+}
+
+function sub(x, y) {
+   return(x - y);
+}
+function mult(x, y) {
+   return(x * y);
+}
+
+function div(x, y) {
+   return(x, y);
+}
+
+sum= sub(6, 8);
+console.log(sum);
